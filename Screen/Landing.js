209b7ca6db33d7126a63cd36e32f8ade9a160bc1
olakike <kolakike59@gmail.com>
2020-12-20 13:15:51 +0100

    modified:   App.js 	modified:   Screen/Landing.js 	new file:   Screen/Landing2.js 	modified:   assets/New folder/Mask Group.png 	new file:   assets/New folder/logo.png

--- Screen/Landing.js
+++ Screen/Landing.js
@@ -1,61 +1,131 @@
+
 import React from 'react';
-import { View, Image, StyleSheet, Text, Button,SafeAreaView} from 'react-native';
+import { View, Image, StyleSheet, Text, Button,SafeAreaView, } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 export default function Landing({navigation}) {
     return (
-    <SafeAreaView style={styles.container}>
-    <View >
-        <View style={styles.logocontainer}>
+    <SafeAreaView>
+    <View style={styles.container} >
+        <View>
+        <Image style={ styles.Logoimage} source={require("../assets/New folder/LOGO.png") } />
         <Image style={ styles.image} source={require("../assets/New folder/Mask Group.png") } />
-        <Text style={ styles.ftext}>  </Text>
+        </View> 
+        <View style={styles.textcontainer}>
+        <Text style={ styles.ftext}> Learning at your own convinience </Text>
+        <Text style={ styles.stext}> Choose the class you belong to get started  </Text>
         </View>
-        <Button style={styles.button} onPress={() => navigation.navigate('Signup')} 
-        title='GET STARTED'
-        color="green"
-        />
+        <View style={ styles.buttoncontainer}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Signup')} >
+        <Text style={styles.buttonText} > STREAM 1 </Text>
+        </TouchableOpacity> 
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Signup')} >
+        <Text style={styles.buttonText} > STREAM 2 </Text>
+        </TouchableOpacity> 
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Signup')} >
+        <Text style={styles.buttonText} > STREAM 3 </Text>
+        </TouchableOpacity> 
+        <View style={styles.landing}>
+            <TouchableOpacity style={styles.swipe} onPress={() => navigation.navigate('Landing')} />
+            <TouchableOpacity style={styles.swipe} onPress={() => navigation.navigate('Landing2')} />
+        </View>
+        <View style={styles.textcontainer}>
+        <Text style={styles.ptext}>Powered by Stream-3 team-2</Text> 
+        </View>
+        </View>
     </View>
     </SafeAreaView>
-
     );
 }
 
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        alignItems: "center",
-        padding:10,
-        backgroundColor: "#000",
+        backgroundColor: "#FFFFFF",
         justifyContent:"center",
-        width:"100%"
+        width:"100%",
+        position:"absolute",
     },
     image:{
-        width:300,
-        height:300,
-        justifyContent:'center',
-        paddingTop: 2,
+        width:408.26,
+        height:408.28,
         alignSelf:"center",
-        borderRadius:"50%"
     },
     ftext:{
-        fontSize: 30,
+        fontSize: 18,
+        lineHeight:23,
+        fontStyle:"normal",
+        width:"280",
+        height:"23",
         alignContent:"center",
         justifyContent:"space-evenly",
-        fontWeight:"bold"      
+        alignSelf:"center",
+        padding:10
     },
-    logocontainer:{
-        marginBottom:200,
-        marginTop:200,
-        justifyContent:"center"
+    stext:{
+        fontSize: 24,
+        justifyContent:"space-evenly",
+        fontWeight:"600",
+        textAlign:"center",
+        lineHeight:30,
+    },
+    ptext:{
+        fontSize: 12,
+        justifyContent:"space-evenly",
+        textAlign:"center",
+        marginTop:"20"
+    },
+    buttonText:{
+        color:"#477A71",
+        fontStyle:'normal',
+        fontWeight:"600",
+        fontSize:"30",
+        textAlign:"center",
+        padding:8
+    },
+    textcontainer:{
+        marginBottom:10,
+        padding: 10
     },
     button: {
-        alignItems: "center",
-        backgroundColor: "green",
-        padding: 10,
-        marginTop:10,
-        width: 300,
-        height: 70,
-        borderRadius:"5%"
+        backgroundColor: "#ffffff",
+        marginBottom:10,
+        width: 290,
+        height: 40,
+        borderRadius:10,
+        bordercolor:"#477A71",
+        borderWidth:3, 
+        alignContent:"center"
+     },
+     swipe: {
+        backgroundColor: "#F0A64A",
+        width: 26,
+        height: 3,
+        bordercolor:"#F0A64A",
+        borderRadius:"10",
+        flexDirection:"row",
+     },
+      buttoncontainer: {
+        backgroundColor:"#ffffff",
+        alignContent:"center",
+        alignSelf:"center"
       },
+      Logoimage:{
+        width: 62 ,
+        height: 64,
+        alignSelf:"center",
+        marginTop:10,
+      },
+      landing:{
+          flexDirection:"row",
+          width:80,
+          alignSelf:"center",
+          marginTop:"10",
+          marginBottom:"10",
+          marginHorizontal:10,
+          justifyContent:"space-evenly",
+         
+      }
 })
 
---
+++ Screen/Landing2.js
@@ -0,0 +1,112 @@
+
+import React from 'react';
+import { View, Image, StyleSheet, Text, Button,SafeAreaView, } from 'react-native';
+import { TouchableHighlight, TouchableOpacity } from 'react-native-gesture-handler';
+
+
+export default function Landing({navigation}) {
+    return (
+    <SafeAreaView style={styles.container}>
+    <View>
+        <Image style={ styles.Logoimage} source={require("../assets/New folder/LOGO.png") } />
+    </View> 
+    <View style={ styles.buttoncontainer}>
+        <Text style={styles.stext} onPress={()=> navigation.navigate("Signup")}> Stream 1</Text>
+        <Text style={styles.ftext} onPress={()=> navigation.navigate("Signup")}> Web Development (Beginners) </Text>
+        <Text style={styles.stext} onPress={()=> navigation.navigate("Signup")}> Stream 2 </Text>
+        <Text style={styles.ftext} onPress={()=> navigation.navigate("Signup")}> Web Development (Advanced) </Text>
+        <Text style={styles.stext} onPress={()=> navigation.navigate("Signup")}> Stream 3 </Text>
+        <Text style={styles.ftext} onPress={()=> navigation.navigate("Signup")}> Mobile App Development 
+            (React native) </Text>
+    </View>
+    <View style={styles.landing}>
+            <TouchableOpacity style={styles.swipe} onPress={() => navigation.navigate('landing2')} />
+            <TouchableOpacity style={styles.swipe} onPress={() => navigation.navigate('landing2')} />   
+    </View>
+    <View style={styles.textcontainer}>
+        <Text style={styles.ptext}>Powered by Stream-3 team-2</Text> 
+    </View>
+    </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: "#477A71",
+        justifyContent:"center",
+        width:"100%",
+        height:"100%"
+    },
+    ftext:{
+        fontSize: 24,
+        lineHeight:30,
+        fontStyle:"normal",
+        width:"355",
+        height:"30",
+        alignContent:"center",
+        justifyContent:"space-evenly",
+        alignSelf:"center",
+        padding:20,
+        color:"#FFFFFF",
+        textAlign:"center"
+    },
+    stext:{
+        fontSize: 36,
+        justifyContent:"space-evenly",
+        fontWeight:"800",
+        textAlign:"center",
+        lineHeight:45,
+        color:"#F0A64A"
+    },
+    ptext:{
+        fontSize: 12,
+        justifyContent:"space-evenly",
+        textAlign:"center",
+        marginTop:"20"
+    },
+    buttonText:{
+        color:"#477A71",
+        fontStyle:'normal',
+        fontWeight:"600",
+        fontSize:"30",
+        textAlign:"center",
+        padding:8
+    },
+    textcontainer:{
+        marginBottom:10,
+        padding: 10
+    },
+     swipe: {
+        backgroundColor: "#F0A64A",
+        width: 26,
+        height: 3,
+        bordercolor:"#F0A64A",
+        borderRadius:"10",
+        flexDirection:"row",
+     },
+      buttoncontainer: {
+        alignContent:"center",
+        alignSelf:"center",
+        paddingVertical:50,
+        marginBottom:50,
+      },
+      Logoimage:{
+        width: 116 ,
+        height: 118,
+        alignSelf:"center",
+        marginTop:30,
+        marginBottom:30
+      },
+      landing:{
+          flexDirection:"row",
+          width:80,
+          alignSelf:"center",
+          marginTop:"10",
+          marginBottom:"10",
+          marginHorizontal:10,
+          justifyContent:"space-evenly",
+         
+      }
+})
+
